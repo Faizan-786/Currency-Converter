1eb5714353afd1bfa87556ac22c4add63c5437d0
Faizan-786 <faizanaliakbar125@gmail.com>
2024-05-17 19:20:13 +0500

    updates

---
+++ index.ts
@@ -0,0 +1,32 @@
+// console.log("Faizan");
+// let myname = "faizan";
+// let position = 12;
+// const result = `${myname} got position ${position}`;
+// console.log(result);
+//  let user = {name: "Faizan" , age: 17}
+//  console.log(user.name);
+
+
+
+let hero;
+
+ function getHero(){
+    return "Hero is Here";
+ }
+
+ hero = getHero();
+ function loginme(name:string, email:string, status:boolean) {
+    if (status) {
+        return `${name} with email ${email} has successfully logged in.`;
+    } else {
+        return `${name} with email ${email} failed to log in.`;
+    }
+}
+
+ let output = loginme("Faizan", "faizan@gmail.com", true);
+ console.log(output);
+
+
+
+//  export{}
+
